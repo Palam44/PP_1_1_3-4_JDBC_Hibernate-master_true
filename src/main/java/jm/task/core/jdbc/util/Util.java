f852ab953a7891fc3f19da3240d088a9e8c65c9c
Palam44 <dpalam44@gmail.com>
2022-08-09 15:23:48 +0300

    Исправлено!!

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -12,7 +12,6 @@ import java.util.Properties;
 
 public class Util {
     private static Connection conn = null;
-    private static Util instance = null;
 
     public static Connection getConnection() {
         try {
